ca4360214e86116e37558c82d1ed589a9e6edece
paolo <paovalletta@hotmail.it>
2015-03-14 11:03:24 +0100

    test jazz eclipse

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
+	<title>PHP connector 4 k12-proj</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 </head>
