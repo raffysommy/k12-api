0b4e6891e0b8f6d2499a3a7ebeaf95330b5723d0
Antonio Russo <antonio@antoniorusso.me>
2015-03-17 18:16:13 -0500

    Ripristino index

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>PHP connector 4 k12-proj</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'><img src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello world!"; ?>
-</h1>
-				<p class='description'></p> Welcome to <span class="blue">K12-project</span> home page. 
-				The site is under costruction!
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+<?php
+               
+        //connetto al db
+        $DBhost = 'us-cdbr-iron-east-01.cleardb.net';
+        $DBuser = 'bf1c9d658ff1ed';
+        $DBpass = '3175e2fb';
+        $DBname = 'ad_5b8045e5aae6ad9';
+        /*$db->doQuery("CREATE TABLE ciao (id INT PRIMARY KEY);");
+        $db->doQuery("INSERT INTO ciao VALUES(1);");*/
+        $mysqli = new MySQLi($DBhost, $DBuser, $DBpass, $DBname);
+        $myQuery = "SELECT * FROM domande ORDER BY RAND() LIMIT 1";
+        $result = $mysqli->query($myQuery) or die($mysqli->error);
+        $json = array();
+
+        if(mysqli_num_rows($result)){//<--- levala, lo fa già il while il controllo
+                while($row=mysqli_fetch_assoc($result)){
+                        $json[]=$row;
+                }
+        }
+        mysqli_close($mysqli);
+        echo json_encode($json);
+?>
